874ac83fafe2f254cb6d928fd476cebcc2abf113
mitema <memm8605@uni.sydney.edu.au>
2019-03-11 16:54:31 +1100

    feat : add major functionalities to card game

--- js/index.js
+++ js/index.js
@@ -36,3 +36,76 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+const cards  = document.getElementsByClassName("card");
+const openCards =  [];
+for(let i=0; i<cards.length; i++){
+        cards[i].addEventListener("click", function(e){
+        if(e.target != null){
+          addToOpenedList(e.target);
+        }
+    });
+}
+
+const addToOpenedList = ((ele) => {
+    openCards.push(ele);
+    checkMatch(openCards);
+});
+
+const checkMatch = ((openCards) =>{
+    let seen = {};
+    let list_length = openCards.length;
+    let firstPos = 0;
+    let countMatch = 0;
+    if (list_length > 1){
+        let i = 0
+        while(i<list_length){
+            list_length -= 1;
+            const property = openCards[i].className.toString();
+            console.log(property);
+            if (seen[property]){
+                lockMatch(firstPos, i);
+                break;
+            }
+            else{
+                seen[property] = true;
+                firstPos = i;
+            }
+            i++;
+        }
+        removeCards(openCards);
+        incCounter();
+        //checkAllMatch()
+    }
+    
+});
+
+//remove unmatched cards
+const removeCards = ((openCards)=>{
+    for(let i =0; i<openCards.length; i++){
+        openCards.pop();
+        openCards[i].style.backgroundColor = "#2e3d49";
+    }
+});
+
+//Increment Counter
+const incCounter = ()=>{
+    let ele = document.getElementById("mv-counter")
+    let val = parseInt(ele.innerHTML.toString());
+    val += 1;
+    ele.innerHTML = val;
+    
+};
+
+
+//lock matched elements (openCards[i], openCards[firstPos])
+const lockMatch = ((firstPos, secondPos)=>{
+    console.log("here");
+    openCards[firstPos].classList.add("match");
+    openCards[secondPos].classList.add("match");
+
+});
+
+//check if all the cards are matched
+
+//const checkAllMatch
